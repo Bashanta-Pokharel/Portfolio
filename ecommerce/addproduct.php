38657ebe97d74da90ee9d7d6854d3947e2c8aa5d
Bashanata62 <pbasanta340@gmail.com>
2026-09-01 21:21:02 +0545

    Update addproduct.php

--- ecommerce/addproduct.php
+++ ecommerce/addproduct.php
@@ -1,7 +1,3 @@
-<?php
-// Name: Bashanta Pokharel, Roll: 62(A)
-include "db.php";
-include "helpers.php";
 
 $product_sql = "SELECT * FROM products ORDER BY id DESC";
 $product_result = mysqli_query($con, $product_sql);
